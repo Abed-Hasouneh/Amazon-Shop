ebd70d555b3f3ff4646bde69dabf7191670aae33
Abdalaleem Hassouna <abdalaleem.hassouna@xngage.com>
2022-10-09 11:42:52 +0300

    adding react bootstrap , product cart

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../actions/productsActions";
+import Product from "../components/Product";
 
 const HomePage = () => {
   const productsList = useSelector((state) => state.productList);
@@ -14,25 +16,16 @@ const HomePage = () => {
 
   return (
     <div>
+      {error && <h2>{error}</h2>}
+      {loading && <h2>Loading ...</h2>}
       <h1>Featured Products</h1>
-      <div className="products">
+      <Row>
         {products.map((product) => (
-          <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info">
-              <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-              </Link>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button>Add to cart</button>
-            </div>
-          </div>
+          <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+            <Product product={product} />
+          </Col>
         ))}
-      </div>
+      </Row>
     </div>
   );
 };
